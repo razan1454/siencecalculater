fdba18e31e433421bb6a38352d66f1a5a6c2f2ca
safa ahmed <safa@gmail.com>
2026-02-18 09:56:07 +0400

    question 3 done

--- projectB.java
+++ projectB.java
@@ -2,7 +2,12 @@ import java.util.Scanner;
 
 public class projectB {
     public static void main (String[] args ){
-        Scanner scanner = new Scanner(System . in);
+
+        Scanner input = new Scanner(System . in);
+        System.out.print("Enter a Number: ");
+        int number = input.nextInt();
+        int square = number * number;
+        System.out.println("Result: " +square);
     }
 
 }
