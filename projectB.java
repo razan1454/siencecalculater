d2b082d179c0311b3c00faa07cbab4f6faca042b
Mariya <lamkia2001@gmail.com>
2026-02-18 09:50:57 +0400

    Exercise number 1 for absolute value solved

--- projectB.java
+++ projectB.java
@@ -3,6 +3,19 @@ import java.util.Scanner;
 public class projectB {
     public static void main (String[] args ){
         Scanner scanner = new Scanner(System . in);
+
+        System.out.println("Enter a Number: ");
+        double input = scanner.nextDouble();
+
+        double result = absoluteVal(input);
+
+        System.out.println("The Result is : "+result);
+
+        scanner.close();
+
+    }
+    public static double absoluteVal(double number){
+        return (number < 0 ) ? -number : number;
     }
 
 }
