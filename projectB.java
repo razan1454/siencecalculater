a81c163bc353cd249b5076cf4e72184a72028f84
Razansalam <rooozalharthi@gmail.com>
2026-02-18 10:08:59 +0400

    Q5

--- projectB.java
+++ projectB.java
@@ -1,8 +1,24 @@
 import java.util.Scanner;
 
 public class projectB {
-    public static void main (String[] args ){
-        Scanner scanner = new Scanner(System . in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number :");
+
+        int input = scanner.nextInt();
+
+        int result = factorial(input);
+        System.out.println("Factorial of " + input + " is: " + result);
+
+
     }
 
+    public static int factorial(int number) {
+        int result = 1;
+        for (int i = number; i > 0; i--) {
+            result *= i;
+        }
+      return result;
+    }
 }
