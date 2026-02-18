6923d18e5d54dcb4f5a109e455e44434f081f94b
TibyanSaad <saad.tibyan02@gmail.com>
2026-02-18 09:49:40 +0400

    even or odd added

--- projectB.java
+++ projectB.java
@@ -1,8 +1,16 @@
 import java.util.Scanner;
 
 public class projectB {
-    public static void main (String[] args ){
-        Scanner scanner = new Scanner(System . in);
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a  number: ");
+        int num1 = scanner.nextInt();
+
+        if (num1 % 2 == 0) {
+            System.out.println("even");
+        }
+        System.out.println("odd");
     }
 
 }
